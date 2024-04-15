db79a37dd5cc7803d0d4e907a462f57a948724e3
Andre Ferreira <andrerhrf@gmail.com>
2024-04-15 10:30:20 -0300

    first commit

---
+++ ByteBuffer.cpp
@@ -0,0 +1,457 @@
+#include "ByteBuffer.h"
+
+FString IntToBase36(int32 Value) {
+    FString chars = TEXT("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    FString result = TEXT("");
+
+    if (Value == 0) 
+        return TEXT("0");
+       
+    while (Value > 0) {
+        int remainder = Value % 36;
+        Value /= 36;
+        result += chars[remainder];
+    }
+    
+    result = result.Reverse();
+
+    return result;
+}
+
+int32 Base36ToInt(const FString& Base36) {
+    return FCString::Strtoi(*Base36, nullptr, 36);
+}
+
+void UBufferData::Initialize(const TMap<FString, FDynamicValue>& InputData)
+{
+    Data = InputData;
+}
+
+FString UBufferData::GetId(FString Key) const {
+    const FDynamicValue* Value = Data.Find(Key);
+
+    if (Value && Value->ValueType == EDynamicValueType::ID)
+        return Value->StringValue;
+    else
+        return FString();
+}
+
+FString UBufferData::GetString(FString Key) const {
+    const FDynamicValue* Value = Data.Find(Key);
+
+    if (Value && Value->ValueType == EDynamicValueType::String)
+        return Value->StringValue;
+    else
+        return FString();
+}
+
+bool UBufferData::GetBool(const FString& Key) const {
+    const FDynamicValue* Value = Data.Find(Key);
+
+    if (Value && Value->ValueType == EDynamicValueType::Bool)
+        return Value->BoolValue;
+
+    return false;
+}
+
+int32 UBufferData::GetInt32(const FString& Key) const {
+    const FDynamicValue* Value = Data.Find(Key);
+
+    if (Value && Value->ValueType == EDynamicValueType::Int32)
+        return Value->IntValue;
+
+    return 0;
+}
+
+uint32 UBufferData::GetUInt32(const FString& Key) const {
+    const FDynamicValue* Value = Data.Find(Key);
+
+    if (Value && Value->ValueType == EDynamicValueType::UInt32)
+        return Value->UIntValue;
+
+    return 0;
+}
+
+float UBufferData::GetFloat(const FString& Key) const {
+    const FDynamicValue* Value = Data.Find(Key);
+
+    if (Value && Value->ValueType == EDynamicValueType::Float)
+        return Value->FloatValue;
+
+    return 0.0;
+}
+
+uint8 UBufferData::GetByte(const FString& Key) const {
+    const FDynamicValue* Value = Data.Find(Key);
+
+    if (Value && Value->ValueType == EDynamicValueType::Byte)
+        return Value->ByteValue;
+
+    return 0;
+}
+
+FVector UBufferData::GetVector(const FString& Key) const {
+    const FDynamicValue* Value = Data.Find(Key);
+
+    if (Value && Value->ValueType == EDynamicValueType::Vector)
+        return Value->VectorValue;
+
+    return FVector(0,0,0);
+}
+
+FRotator UBufferData::GetRotator(const FString& Key) const {
+    const FDynamicValue* Value = Data.Find(Key);
+
+    if (Value && Value->ValueType == EDynamicValueType::Rotator)
+        return Value->RotatorValue;
+
+    return FRotator(0, 0, 0);
+}
+
+void UByteBuffer::EnsureCapacity(int32 RequiredBytes)
+{
+    int32 RequiredCapacity = Position + RequiredBytes;
+
+    if (RequiredCapacity > Buffer.Max())
+        Buffer.Reserve(RequiredCapacity);
+}
+
+UByteBuffer* UByteBuffer::CreateEmptyByteBuffer()
+{
+    UByteBuffer* ByteBuffer = NewObject<UByteBuffer>();
+    ByteBuffer->Buffer = TArray<uint8>();
+    ByteBuffer->Position = 0;
+    return ByteBuffer;
+}
+
+UByteBuffer* UByteBuffer::CreateByteBuffer(const TArray<uint8>& Data = TArray<uint8>())
+{
+    UByteBuffer* ByteBuffer = NewObject<UByteBuffer>();
+    ByteBuffer->Buffer = Data;
+    ByteBuffer->Position = 0;
+    return ByteBuffer;
+}
+
+UByteBuffer* UByteBuffer::CreateByteBufferFromString(const FString& Base64Data)
+{
+    UByteBuffer* ByteBuffer = NewObject<UByteBuffer>();
+
+    TArray<uint8> DecodedData;
+    FBase64::Decode(Base64Data, DecodedData);
+
+    ByteBuffer->Buffer = DecodedData;
+    ByteBuffer->Position = 0;
+
+    return ByteBuffer;
+}
+
+FString UByteBuffer::GetId()
+{
+    int32 IdValue = GetInt32();
+    FString value = IntToBase36(IdValue);
+        
+    return value;
+}
+
+UByteBuffer* UByteBuffer::PutId(const FString& Id)
+{
+    int32 IdValue = Base36ToInt(Id);
+    return PutInt32(IdValue);
+}
+
+UByteBuffer* UByteBuffer::PutInt32(int32 Value)
+{
+    EnsureCapacity(4);
+
+    Buffer.Add(Value & 0xFF);              
+    Buffer.Add((Value >> 8) & 0xFF);        
+    Buffer.Add((Value >> 16) & 0xFF);       
+    Buffer.Add((Value >> 24) & 0xFF);       
+
+    Position += 4;
+
+    return this;
+}
+
+UByteBuffer* UByteBuffer::PutUInt32(uint32 Value)
+{
+    EnsureCapacity(4);
+
+    Buffer.Add(Value & 0xFF);
+    Buffer.Add((Value >> 8) & 0xFF);
+    Buffer.Add((Value >> 16) & 0xFF);
+    Buffer.Add((Value >> 24) & 0xFF);
+
+    Position += 4;
+
+    return this;
+}
+
+int32 UByteBuffer::GetInt32()
+{
+    if (Buffer.Num() >= Position + 4) {
+        int32 result = 0;
+
+        result |= static_cast<int32>(Buffer[Position]) << 0;
+        result |= static_cast<int32>(Buffer[Position + 1]) << 8;
+        result |= static_cast<int32>(Buffer[Position + 2]) << 16;
+        result |= static_cast<int32>(Buffer[Position + 3]) << 24;
+
+        Position += 4;
+
+        return result;
+    }
+    else {
+        UE_LOG(LogTemp, Error, TEXT("Erro no pacote %d"), Packet);
+        return 0;
+    }
+}
+
+uint32 UByteBuffer::GetUInt32()
+{
+    uint32 result = 0;
+
+    result |= static_cast<uint32>(Buffer[Position]) << 0;
+    result |= static_cast<uint32>(Buffer[Position + 1]) << 8;
+    result |= static_cast<uint32>(Buffer[Position + 2]) << 16;
+    result |= static_cast<uint32>(Buffer[Position + 3]) << 24;
+
+    Position += 4;
+
+    return result;
+}
+
+UByteBuffer* UByteBuffer::PutByte(uint8 Value)
+{
+    EnsureCapacity(1);
+    Buffer.Add(Value);
+    Position += 1;
+    return this;
+}
+
+uint8 UByteBuffer::GetByte()
+{
+    uint8 Value = Buffer[Position];
+    Position += 1;
+    return Value;
+}
+
+UByteBuffer* UByteBuffer::PutString(const FString& Value)
+{
+    FTCHARToUTF8 Convert(*Value); 
+    int32 Length = Convert.Length();
+    EnsureCapacity(4 + Length); 
+    PutInt32(Length);
+
+    for (int32 i = 0; i < Length; ++i)    
+        PutByte((uint8)Convert.Get()[i]);
+
+    return this;
+}
+
+FString UByteBuffer::GetString()
+{
+    int32 Length = GetInt32();
+
+    if (Length > 0 && Position + Length <= Buffer.Num())
+    {
+        TArray<uint8> StringBytes;
+        StringBytes.Append(&Buffer[Position], Length);
+        
+        Position += Length;
+
+        FString Result = FString(FUTF8ToTCHAR(reinterpret_cast<const ANSICHAR*>(StringBytes.GetData()), StringBytes.Num()));
+
+        return Result;
+    }
+
+    return FString();
+}
+
+UByteBuffer* UByteBuffer::PutFloat(float Value) {
+    EnsureCapacity(sizeof(float));
+    FMemory::Memcpy(Buffer.GetData() + Position, &Value, sizeof(float));
+    Position += sizeof(float);
+    return this;
+}
+
+float UByteBuffer::GetFloat() {
+    if (Position + sizeof(float) > Buffer.Num()) 
+        return 0.0f;
+    
+    float Value;
+    FMemory::Memcpy(&Value, Buffer.GetData() + Position, sizeof(float));
+    Position += sizeof(float);
+    return Value;
+}
+
+UByteBuffer* UByteBuffer::PutBool(bool Value)
+{
+    EnsureCapacity(1);
+    uint8 ByteValue = Value ? 1 : 0;
+    Buffer.Add(ByteValue);
+    Position += 1;
+    return this;
+}
+
+bool UByteBuffer::GetBool()
+{
+    if (Position + 1 > Buffer.Num())
+        return false;
+    
+    bool Value = Buffer[Position] != 0;
+    Position += 1;
+    return Value;
+}
+
+UByteBuffer* UByteBuffer::PutVector(const FVector& Value)
+{
+    PutFloat(Value.X);
+    PutFloat(Value.Y);
+    PutFloat(Value.Z);
+    return this;
+}
+
+FVector UByteBuffer::GetVector()
+{
+    FVector Value;
+    Value.X = GetFloat();
+    Value.Y = GetFloat();
+    Value.Z = GetFloat();
+    return Value;
+}
+
+UByteBuffer* UByteBuffer::PutRotator(const FRotator& Value)
+{
+    PutFloat(Value.Pitch);
+    PutFloat(Value.Yaw);
+    PutFloat(Value.Roll);
+    return this;
+}
+
+FRotator UByteBuffer::GetRotator()
+{
+    FRotator Value;
+    Value.Pitch = GetFloat();
+    Value.Yaw = GetFloat();
+    Value.Roll = GetFloat();
+    return Value;
+}
+
+bool UByteBuffer::ReadDataFromBuffer(const TMap<FString, FString>& DataSequence, UBufferData*& OutValues, uint8 PacketID)
+{
+    TMap<FString, FDynamicValue> Values;
+    Values.Empty();
+    Packet = PacketID;
+
+    for (const TPair<FString, FString>& Elem : DataSequence)
+    {
+        FDynamicValue NewValue;
+
+        if (Elem.Value == FString("id"))
+        {
+            NewValue.ValueType = EDynamicValueType::ID;
+            NewValue.StringValue = GetId();
+        }
+        else if (Elem.Value == FString("int32") || Elem.Value == FString("int"))
+        {
+            NewValue.ValueType = EDynamicValueType::Int32;
+            NewValue.IntValue = GetInt32();
+        }
+        else if (Elem.Value == FString("uint32") || Elem.Value == FString("uint"))
+        {
+            NewValue.ValueType = EDynamicValueType::UInt32;
+            NewValue.IntValue = GetUInt32();
+        }
+        else if (Elem.Value == FString("float"))
+        {
+            NewValue.ValueType = EDynamicValueType::Float;
+            NewValue.FloatValue = GetFloat();
+        }
+        else if (Elem.Value == FString("string") || Elem.Value == FString("str"))
+        {
+            UE_LOG(LogTemp, Log, TEXT("Get String"));
+            NewValue.ValueType = EDynamicValueType::String;
+            NewValue.StringValue = GetString();
+        }
+        else if (Elem.Value == FString("byte"))
+        {
+            NewValue.ValueType = EDynamicValueType::Byte;
+            NewValue.ByteValue = GetByte();
+        }
+        else if (Elem.Value == FString("bool") || Elem.Value == FString("boolean"))
+        {
+            NewValue.ValueType = EDynamicValueType::Bool;
+            NewValue.BoolValue = GetBool();
+        }
+        else if (Elem.Value == FString("vector"))
+        {
+            NewValue.ValueType = EDynamicValueType::Vector;
+            NewValue.VectorValue = GetVector();
+        }
+        else if (Elem.Value == FString("rotator"))
+        {
+            NewValue.ValueType = EDynamicValueType::Rotator;
+            NewValue.RotatorValue = GetRotator();
+        }
+
+        Values.Add(Elem.Key, NewValue);
+    }
+
+    OutValues = NewObject<UBufferData>();
+    OutValues->Initialize(Values);
+
+    return true;
+}
+
+void UByteBuffer::WriteDataToBuffer(const TMap<FString, FString>& DataSequence, const TArray<FDynamicValue>& Values)
+{
+    int32 ValueIndex = 0; 
+
+    for (const TPair<FString, FString>& Elem : DataSequence)
+    {
+        if (ValueIndex >= Values.Num())        
+            break;
+        
+        const FDynamicValue& CurrentValue = Values[ValueIndex++];
+
+        if (Elem.Value.Equals("id"))
+            PutId(CurrentValue.StringValue);
+        else if (Elem.Value.Equals("int32") || Elem.Value.Equals("int"))
+            PutInt32(CurrentValue.IntValue);
+        else if (Elem.Value.Equals("uint32") || Elem.Value.Equals("uint"))
+            PutInt32(CurrentValue.UIntValue);
+        else if (Elem.Value.Equals("float"))
+            PutFloat(CurrentValue.FloatValue);
+        else if (Elem.Value.Equals("string") || Elem.Value.Equals("str"))
+            PutString(CurrentValue.StringValue);
+        else if (Elem.Value.Equals("byte"))
+            PutByte(CurrentValue.ByteValue);
+        else if (Elem.Value.Equals("bool") || Elem.Value.Equals("boolean"))
+            PutBool(CurrentValue.BoolValue);
+        else if (Elem.Value.Equals("vector"))
+            PutVector(CurrentValue.VectorValue);
+        else if (Elem.Value.Equals("rotator"))
+            PutRotator(CurrentValue.RotatorValue);
+    }
+}
+
+FString UByteBuffer::ToString() const
+{
+    return FBase64::Encode(Buffer);
+}
+
+TArray<uint8>& UByteBuffer::GetBuffer() {
+    return Buffer;
+}
+
+int32 UByteBuffer::Length() {
+    return Buffer.Num();
+}
+
+void UByteBuffer::AppendBuffer(UByteBuffer* OtherBuffer)
+{
+    Buffer.Reserve(Buffer.Num() + OtherBuffer->Buffer.Num());
+    Buffer.Append(OtherBuffer->Buffer);
+    Position = Buffer.Num();
+}
---
+++ ByteBuffer.h
@@ -0,0 +1,185 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Misc/Base64.h"
+#include "ByteBuffer.generated.h"
+
+UENUM(BlueprintType)
+enum class EDynamicValueType : uint8
+{
+	ID,
+	Int32,
+	UInt32,
+	Float,
+	Bool,
+	String,
+	Byte,
+	Vector,
+	Rotator,
+	None
+};
+
+USTRUCT(BlueprintType)
+struct FDynamicValue
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ByteBuffer")
+	EDynamicValueType ValueType = EDynamicValueType::None;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ByteBuffer")
+	int32 IntValue;
+
+	uint32 UIntValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ByteBuffer")
+	float FloatValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ByteBuffer")
+	bool BoolValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ByteBuffer")
+	FString StringValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ByteBuffer")
+	uint8 ByteValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ByteBuffer")
+	FVector VectorValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ByteBuffer")
+	FRotator RotatorValue;
+};
+
+UCLASS(BlueprintType)
+class CLIENT_API UBufferData : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	void Initialize(const TMap<FString, FDynamicValue>& InputData);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	FString GetId(FString Key) const;
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	FString GetString(FString Key) const;
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	bool GetBool(const FString& Key) const;
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	int32 GetInt32(const FString& Key) const;
+
+	uint32 GetUInt32(const FString& Key) const;
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	float GetFloat(const FString& Key) const;
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	uint8 GetByte(const FString& Key) const;
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	FVector GetVector(const FString& Key) const;
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	FRotator GetRotator(const FString& Key) const;
+
+private:
+	TMap<FString, FDynamicValue> Data;
+};
+
+UCLASS(BlueprintType)
+class CLIENT_API UByteBuffer : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, Category = "ByteBuffer")
+	static UByteBuffer* CreateEmptyByteBuffer();
+
+	UFUNCTION(BlueprintPure, Category = "ByteBuffer")
+	static UByteBuffer* CreateByteBuffer(const TArray<uint8>& Data);
+	
+	UFUNCTION(BlueprintPure, Category = "ByteBuffer")
+	static UByteBuffer* CreateByteBufferFromString(const FString& Base64Data);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	UByteBuffer* PutId(const FString& Id);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	FString GetId();
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	UByteBuffer* PutInt32(int32 Value);
+
+	UByteBuffer* PutUInt32(uint32 Value);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	int32 GetInt32();
+
+	uint32 GetUInt32();
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	UByteBuffer* PutByte(uint8 Value);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	uint8 GetByte();
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	UByteBuffer* PutString(const FString& Value);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	FString GetString();
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	UByteBuffer* PutFloat(float Value);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	float GetFloat();
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	UByteBuffer* PutBool(bool Value);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	bool GetBool();
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	UByteBuffer* PutVector(const FVector& Value);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	FVector GetVector();
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	UByteBuffer* PutRotator(const FRotator& Value);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	FRotator GetRotator();
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	bool ReadDataFromBuffer(const TMap<FString, FString>& DataSequence, UBufferData*& OutValues, uint8 PacketID);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	void WriteDataToBuffer(const TMap<FString, FString>& DataSequence, const TArray<FDynamicValue>& Values);
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	FString ToString() const;
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	TArray<uint8>& GetBuffer();
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	int32 Length();
+
+	UFUNCTION(BlueprintCallable, Category = "ByteBuffer")
+	void AppendBuffer(UByteBuffer* OtherBuffer);
+		
+private:
+	TArray<uint8> Buffer;
+	int32 Position = 0;
+	uint8 Packet = 0;
+
+	void EnsureCapacity(int32 RequiredBytes);
+};
